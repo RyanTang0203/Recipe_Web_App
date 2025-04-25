5ff5cba255a4b27ecd1fbfa59e0908b32b1530c4
yuzhen1713 <yuzhen.kuang@sjsu.edu>
2025-04-25 13:21:40 -0700

    Update format for recipe

--- app/models.py
+++ app/models.py
@@ -8,3 +8,10 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
