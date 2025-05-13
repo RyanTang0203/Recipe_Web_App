1e9a312de73ec27a666ff14f8127a39db6a662ff
yuzhen1713 <yuzhen.kuang@sjsu.edu>
2025-05-12 20:23:44 -0700

    Update models.py to make sure save recipe into database.

--- app/models.py
+++ app/models.py
@@ -3,12 +3,23 @@ from flask_login import UserMixin
 from datetime import datetime
 
 db = SQLAlchemy()
+saved_recipes = db.Table(
+    'saved_recipes',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True)
+)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     recipes = db.relationship('Recipe', backref='user')
+    saved = db.relationship(
+        'Recipe',
+        secondary=saved_recipes,
+        backref=db.backref('saved_by', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
 
 class Recipe(db.Model):
